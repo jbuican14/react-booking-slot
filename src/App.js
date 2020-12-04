a9b24eb9bc1df65457496b3c49b781c32373408e
Bew <Bew@sysops-MBP.broadband>
2020-12-04 10:54:57 +0000

    Initial commit, add 2 components (slot and calendar)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import Slot from './components/Slot.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="heading">Choose your delivery day</h1>
+      <p className="sub-heading">Delivery is always free</p>
+      <Slot />
     </div>
   );
 }
---
+++ src/components/Slot.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Calendar from './Calendar.component';
+
+export default function Slot() {
+  return (
+    <>
+      <div>
+        <p>Thursday March 14 </p>
+        <span>Earliest delivery</span>
+        <img src="" />
+        <button>Change</button>
+      </div>
+
+      <Calendar />
+    </>
+  );
+}
